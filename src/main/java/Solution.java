db7da5b246f4a01661b0aeeedaf8d3a8820e2616
alek <wait82985@gmail.com>
2026-02-07 09:32:48 +0500

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,26 +1,21 @@
 class Solution {
     public int minimumDeletions(String s) {
-        Integer[][] memo = new Integer[s.length() + 1][2];
-        return dfs(s, 0, 1, memo);
-    }
-
-    int dfs(String s, int i, int a, Integer[][] memo) {
-        if (i == s.length()) {
-            return 0;
+        int n = s.length();
+        int res = n;
+        int a = 0;
+        int b = 0;
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == 'a') {
+                a++;
+            }
         }
-        if (memo[i][a] != null) {
-            return memo[i][a];
+        for (int i = 0; i < n; i++) {
+            res = Math.min(res, a + b);
+            if (s.charAt(i) == 'a') {
+                a--;
+            } else b++;
         }
-        int res = Integer.MAX_VALUE;
-        char cur = s.charAt(i);
-        if ((cur == 'a' && a == 1) || (cur == 'b' && a == 0)) {
-            res = dfs(s, i + 1, a, memo);
-        } else if (cur == 'b' && a == 1) {
-            res = Math.min(dfs(s, i + 1, 0, memo), dfs(s, i + 1, a, memo) + 1);
-        } else if (cur == 'a' && a == 0) {
-            res = dfs(s, i + 1, 0, memo) + 1;
-        }
-        memo[i][a] = res;
+        res = Math.min(res, a + b);
         return res;
     }
 }
